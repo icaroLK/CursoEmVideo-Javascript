1f716745503edb43c116be9aaa7bff4ab8b74f08
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-05-28 23:28:10 -0300

    nice

---
+++ Modulo01/aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 50
+console.log(`O carro estava andando a ${vel} km/h`)
+
+if (vel >= 60){
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO   `)
+}
+else{
+    comando 2
+}
+
+
+console.log(`Utilize o sinto :)`)
